f5065c6214dcc2791212b80cadfb3871dc766953
Jurandy Soares <jurandy.soares@gmail.com>
2021-03-29 11:01:48 -0300

    Deployed b23c3d5 with MkDocs version: 1.1.2

--- README.md
+++
@@ -1,2 +0,0 @@
-# josalab.github.io
-Laboratório digital da escola Josafá Sisino Machado
--- atualiza-site.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-(cd ../josalab; git pull)
-mkdocs gh-deploy --config-file ../josalab/mkdocs.yml --remote-branch main
